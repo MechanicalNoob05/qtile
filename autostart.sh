6775a11ebfa7344e414d575f5a2e66739c37b6b8
Tejas mayekar <tejasmayekar13@gmail.com>
2023-05-27 22:48:50 +0530

    power menu

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 greenclip clear &       # Clear Green clip History 
 nitrogen --restore &    # Wallpaper
-cbatticon &             # Battery Icon
 lxsession &             # Session Manager
 picom &                 # Compositor
 dunst &                 # Notification Manager
